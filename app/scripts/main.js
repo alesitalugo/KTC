2349c9a563cfb826835c876fb7a4d959c9fe2d20
alejandra muciño <alita@MacBook-Air-de-alejandra.local>
2015-01-25 03:25:32 -0600

    hammer fail

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,16 @@
 'use strict';
 /* jshint camelcase: false */
+
+
 var Slider = function(el){
-	var slides = $(el).find('.item_slider');
+	var slides = $(el).find('.item_gall');
 	var slide_total = slides.length;
 	var slide_actual = 0;
 
 
 	return{
 		'init': function(){
-			slides.first().addClass('active_slide');
+			slides.first().addClass('showtime');
 		},
 		'next_slide': function(){
 			if( slide_actual + 1 < slide_total ){
@@ -27,17 +29,24 @@ var Slider = function(el){
 	};
 };
 
-
-var slider_home = new Slider( document.getElementById('slider') );
+var slider_home = new Slider( document.getElementById('gall') );
 
 slider_home.init();
-$('.nav_item').on('click', function( e ){
+
+$('#left_gall').on('click', function( e ){
 	e.preventDefault();
-	if( $(this).hasClass('next') ){
-		slider_home.next_slide();
-	}
-	if( $(this).hasClass('prev') ){
-		slider_home.prev_slide();
-	}
-	return false;
+	slider_home.next_slide();
 });
+$('#right_gall').on('click', function( e ){
+	e.preventDefault();
+	slider_home.prev_slide();
+});
+	var mc = new Hammer(slider_home);
+	
+	mc.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+
+	// listen to events...
+	mc.on("swipeleft", function(ev) {
+	   
+	    console.log('left');
+	});
