069bdc63f548520369db3de04733bf640e4ab1c8
alesitalugo <alesitalugo@gmail.com>
2015-01-25 23:30:12 -0600

    final

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,13 @@
-// Generated on 2015-01-22 using generator-webapp 0.4.9
 'use strict';
-
-// # Globbing
-// for performance reasons we're only matching one level down:
-// 'test/spec/{,*/}*.js'
-// use this if you want to recursively match all subfolders:
-// 'test/spec/**/*.js'
-
 module.exports = function (grunt) {
-
-    // Load grunt tasks automatically
     require('load-grunt-tasks')(grunt);
-
-    // Time how long tasks take. Can help when optimizing build times
     require('time-grunt')(grunt);
-
-    // Configurable paths
     var config = {
         app: 'app',
-        dist: 'dist'
+        dist: 'build'
     };
-
-    // Define the configuration for all the tasks
     grunt.initConfig({
-
-        // Project settings
         config: config,
-
-        // Watches files for changes and runs tasks based on the changed files
         watch: {
             bower: {
                 files: ['bower.json'],
@@ -66,14 +46,11 @@ module.exports = function (grunt) {
                 ]
             }
         },
-
-        // The actual grunt server settings
         connect: {
             options: {
                 port: 9000,
                 open: true,
                 livereload: 35729,
-                // Change this to '0.0.0.0' to access the server from outside
                 hostname: 'localhost'
             },
             livereload: {
@@ -108,8 +85,6 @@ module.exports = function (grunt) {
                 }
             }
         },
-
-        // Empties folders to start fresh
         clean: {
             dist: {
                 files: [{
@@ -123,8 +98,6 @@ module.exports = function (grunt) {
             },
             server: '.tmp'
         },
-
-        // Make sure code styles are up to par and there are no obvious mistakes
         jshint: {
             options: {
                 jshintrc: '.jshintrc',
@@ -137,8 +110,6 @@ module.exports = function (grunt) {
                 'test/spec/{,*/}*.js'
             ]
         },
-
-        // Mocha testing framework configuration options
         mocha: {
             all: {
                 options: {
@@ -147,8 +118,6 @@ module.exports = function (grunt) {
                 }
             }
         },
-
-        // Compiles Sass to CSS and generates necessary files if requested
         sass: {
             options: {
                 includePaths: [
@@ -174,8 +143,6 @@ module.exports = function (grunt) {
                 }]
             }
         },
-
-        // Add vendor prefixed styles
         autoprefixer: {
             options: {
                 browsers: ['last 1 version']
@@ -189,8 +156,6 @@ module.exports = function (grunt) {
                 }]
             }
         },
-
-        // Automatically inject Bower components into the HTML file
         bowerInstall: {
             app: {
                 src: ['<%= config.app %>/index.html'],
@@ -200,8 +165,6 @@ module.exports = function (grunt) {
                 src: ['<%= config.app %>/styles/{,*/}*.{scss,sass}']
             }
         },
-
-        // Renames files for browser caching purposes
         rev: {
             dist: {
                 files: {
@@ -215,18 +178,12 @@ module.exports = function (grunt) {
                 }
             }
         },
-
-        // Reads HTML for usemin blocks to enable smart builds that automatically
-        // concat, minify and revision files. Creates configurations in memory so
-        // additional tasks can operate on them
         useminPrepare: {
             options: {
                 dest: '<%= config.dist %>'
             },
             html: '<%= config.app %>/index.html'
         },
-
-        // Performs rewrites based on rev and the useminPrepare configuration
         usemin: {
             options: {
                 assetsDirs: ['<%= config.dist %>', '<%= config.dist %>/images']
@@ -234,8 +191,6 @@ module.exports = function (grunt) {
             html: ['<%= config.dist %>/{,*/}*.html'],
             css: ['<%= config.dist %>/styles/{,*/}*.css']
         },
-
-        // The following *-min tasks produce minified files in the dist folder
         imagemin: {
             dist: {
                 files: [{
@@ -246,7 +201,6 @@ module.exports = function (grunt) {
                 }]
             }
         },
-
         svgmin: {
             dist: {
                 files: [{
@@ -257,7 +211,6 @@ module.exports = function (grunt) {
                 }]
             }
         },
-
         htmlmin: {
             dist: {
                 options: {
@@ -278,34 +231,6 @@ module.exports = function (grunt) {
                 }]
             }
         },
-
-        // By default, your `index.html`'s <!-- Usemin block --> will take care of
-        // minification. These next options are pre-configured if you do not wish
-        // to use the Usemin blocks.
-        // cssmin: {
-        //     dist: {
-        //         files: {
-        //             '<%= config.dist %>/styles/main.css': [
-        //                 '.tmp/styles/{,*/}*.css',
-        //                 '<%= config.app %>/styles/{,*/}*.css'
-        //             ]
-        //         }
-        //     }
-        // },
-        // uglify: {
-        //     dist: {
-        //         files: {
-        //             '<%= config.dist %>/scripts/scripts.js': [
-        //                 '<%= config.dist %>/scripts/scripts.js'
-        //             ]
-        //         }
-        //     }
-        // },
-        // concat: {
-        //     dist: {}
-        // },
-
-        // Copies remaining files to places other tasks can use
         copy: {
             dist: {
                 files: [{
@@ -330,8 +255,6 @@ module.exports = function (grunt) {
                 src: '{,*/}*.css'
             }
         },
-
-        // Run some tasks in parallel to speed up build process
         concurrent: {
             server: [
                 'sass:server',
@@ -343,12 +266,9 @@ module.exports = function (grunt) {
             dist: [
                 'sass',
                 'copy:styles'
-                //'imagemin',
-                //'svgmin'
             ]
         }
     });
-
 
     grunt.registerTask('serve', function (target) {
         if (target === 'dist') {
@@ -395,7 +315,6 @@ module.exports = function (grunt) {
         'copy:dist',
         'rev',
         'usemin'
-        //'htmlmin'
     ]);
 
     grunt.registerTask('default', [
--- README.md
+++ README.md
@@ -1,10 +1,27 @@
 # KTC
+![KTC](http://i.imgur.com/Gx90AFO.png)
 
+Test project for the KTC Agency.
+
+## Instalation
+To only see the static files of the build we can direct download [from here](https://codeload.github.com/alesitalugo/KTC/zip/gh-pages) and then open the `index.html` file from our browser.
+
+Or,
+
+We can clone the repository then go to the `build` directory and run a simple server with python.
+
+```
+git clone git@github.com:alesitalugo/KTC.git
+cd KTC/build
+python -m SimpleHTTPServer
+```
+
+And open `http://0.0.0.0:8000/` on our favourite server.
 
 ## Dev Environment
-To run dev environment we need to have [Node.js](nodejs.org) installed.
+To run dev environment we have to clone the project and we need to have [Node.js](nodejs.org) installed.
 
-Also we need to install the package manager Bower and the task manager Grunt with:
+Then we need to install the package manager Bower and the task manager Grunt with:
 
 ```
 npm install -g bower grunt-cli
@@ -21,3 +38,21 @@ Now we can execute the Gruntfile to run the dev envitonment:
 ```
 grunt serve
 ```
+
+To build a version of our site we can execute:
+
+```
+grunt build
+```
+
+## License
+
+This code is released under the Beerware License:
+
+> "THE BEER-WARE LICENSE" (Revision 42):
+>
+> [Alejandra Lugo](https://github.com/alesitalugo) wrote this file.  As long as you retain this notice you
+> can do whatever you want with this stuff. If we meet some day, and you think
+> this stuff is worth it, you can buy me a beer in return.
+>
+> Alejandra Lugo - @alesitalugo.
